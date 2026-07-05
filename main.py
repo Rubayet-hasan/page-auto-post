de4e5653c0362daf2e322ec964182faca5abfe09
Md Rubayet <mdrubayet27@gmail.com>
2026-07-05 14:12:51 +0600

    main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from threading import Thread
 from flask import Flask
 
 # ==================== CONFIGURATION ====================
-FB_PAGE_ID = os.environ.get("FB_PAGE_ID", "1096048570267653")
+FB_PAGE_ID = os.environ.get("FB_PAGE_ID", "1711935760121284")
 FB_PAGE_TOKEN = os.environ.get("FB_PAGE_TOKEN", "EAAYUZCxrcPcQBR6HUyQXY3EdSEJ5xz6w5yNGPN3lOkPAZB11WQPCWXZCspCJoS5R7o7fxSLYbJlu9ZCuImMpw0UjbwV0d6T2Nu3L6PK0MUTTlmJbVrMTzpNM26wcZAM3tzaPOw4DfdyfWRR0kaZCYu7SWALmb6OeaLW74rArPFJXCf8VZB5Csxs3ILFUrPcT0CZBwoTzfpY8")
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "AQ.Ab8RN6KDm0FDEDwnh-vAsuzj8nVpvJGO43B92X7i-aBB9WMiaA")
 # =======================================================
